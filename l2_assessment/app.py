a2afae6ff819d14a9cfa328cbd0bb7c998fc3c74
Eve Tinker <et22@mahurangi.school.nz>
2026-06-03 13:13:10 +1200

    Added the python code for the search bar

--- l2_assessment/app.py
+++ l2_assessment/app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/search")
+def search():
+    query = request.args.get("query")
+
+    conn = sqlite3.connect("myth_database.db")
+    cursor = conn.cursor
+
+    cursor.execute("""
+        SELECT *
+        FROM creatures
+        WHERE [Mythical Creature Name] LIKE?
+    """, (f"%{query}%",))
+
+    results = cursor.fetchall()
+
+    conn.close()
+
+    return render_template(
+        "search_results.html",
+        query=query,
+        results=results
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
